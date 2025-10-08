a01a816fb2340febb8dda8f6865f919ae6354405
Fatima Boumsahi <fatima.boumsahi.23@ump.ac.ma>
2025-10-08 17:29:50 +0100

    first push

---
+++ script.js
@@ -0,0 +1,9 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_a7w3d46","template_fqwf28y",parms).then(alert("Your email is send !"))
+}
