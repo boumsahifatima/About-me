044d5cf65b2860efecafb9f8ea55528dda1bc03b
Fatima Boumsahi <fatima.boumsahi.23@ump.ac.ma>
2025-10-09 23:08:26 +0100

    Update script.js
    Signed-off-by: Fatima Boumsahi <fatima.boumsahi.23@ump.ac.ma>

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ function sendMail(){
         message : document.getElementById("message").value,
     }
 
+    
+
     emailjs.send("service_a7w3d46","template_fqwf28y",parms).then(alert("Your email is send !"))
+
 }
